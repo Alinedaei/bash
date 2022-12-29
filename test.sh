a2f6bcdd4a8d510cce923b58c84bbf23f0c1445e
Alinedaei <86824959+Alinedaei@users.noreply.github.com>
2022-12-29 13:17:01 +0330

    first commit

---
+++ test.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "enter file name: \c"
+reade filename
+
+if 
---
+++ test1.sh
@@ -0,0 +1,9 @@
+#!/bin/bash 
+ix=12
+
+if (( $x >= 10 )) && (( $x <= 20 ))
+then 
+	echo "x is in renge"	
+else
+       echo "x is not in renge"
+fi 
